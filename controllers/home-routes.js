b08752fdfcf5f194c6ade0c25d8c998372855a0b
JoshMiller5994 <talk2joshbgosh@gmail.com>
2020-10-29 17:47:15 -0600

    added the login page layout

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -5,4 +5,22 @@ router.get('/', (req, res) => {
     res.render('homepage');
 });
 
+router.get('/login', (req, res) => {
+    if(req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('login');
+});
+
+router.get('/dashboard', (req, res) => {
+    if(req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('dashboard');
+})
+
 module.exports = router;
