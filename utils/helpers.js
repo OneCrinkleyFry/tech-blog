1158bedcc5b9a0d506c0972fe8d1f70d2c8c034b
JoshMiller5994 <talk2joshbgosh@gmail.com>
2020-10-31 18:22:09 -0600

    every API endpoint functions perfectly

--- utils/helpers.js
+++ utils/helpers.js
@@ -8,13 +8,5 @@ module.exports = {
         }
 
         return word;
-    },
-    format_url: url => {
-        return url
-            .replace('https://', '')
-            .replace('http://', '')
-            .replace('www.', '')
-            .split('/')[0]
-            .split('?')[0];
     }
 }
