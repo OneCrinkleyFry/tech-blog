d0273896bd7f9ad9b4a539b8f5c194f492bfe254
JoshMiller5994 <talk2joshbgosh@gmail.com>
2020-11-01 21:42:46 -0700

    displayed the comments

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -1,17 +1,18 @@
 async function commentFormHandler(event) {
     event.preventDefault();
 
-    const comment_text = document.querySelector('textarea[name = "comment-body"]').value.trim();
+    const comment_text = document.querySelector('textarea[name = "new-comment-text"]').value.trim();
 
-    const post_id = window.location.toString().split('/')[
-        window.location.toString().split('/').length - 1
-    ];
+    const post_id = window.location.toString()
+        .split('/')[window.location.toString().split('/').length - 1]
+        .split('?')[0];
 
+    console.log(comment_text, post_id)
     if (comment_text) {
         const response = await fetch('/api/comments', {
             method: 'POST',
             body: JSON.stringify({ post_id, comment_text }),
-            headers: {  'Content-Type': 'application/json' }
+            headers: { 'Content-Type': 'application/json' }
         });
 
         if (response.ok) document.location.reload();
@@ -19,4 +20,4 @@ async function commentFormHandler(event) {
     }
 }
 
-document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
+document.querySelector('.new-comment-form').addEventListener('submit', commentFormHandler);
